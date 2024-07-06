d441fefd90fae54b478d8ed21ccb5ad234164d1c
Oleg Schegol <olschegolev@yahoo.com>
2024-07-06 18:34:29 +0500

    Auth token

--- app/src/main/java/ru/olschegolev/retrofitgetandpost/ApiService.java
+++ app/src/main/java/ru/olschegolev/retrofitgetandpost/ApiService.java
@@ -7,15 +7,29 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface ApiService {
+
+
     @GET("/posts")
     Call<List<Post>> getPosts();
+
+    @GET("/posts")
+    Call<List<Post>> getPostsApi(@Header("Authorization") String authToken);
 
     @POST("/posts")
     @FormUrlEncoded
     Call<Post> createPost(@Field("title") String title,
                           @Field("body") String body,
                           @Field("userId") int userid);
+
+    @POST("/posts")
+    @FormUrlEncoded
+    Call<Post> createPostApi(
+            @Header("Authorization") String authToken,
+            @Field("title") String title,
+            @Field("body") String body,
+            @Field("userId") int userid);
 }
